ce1cfef82f73f0d5e89e9912437b9a86dc89a714
PRZEMEK <a.przmeke@o2.pl>
2021-07-21 15:21:56 +0200

    last one

---
+++ src/ug/Start.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ug;
+
+/**
+ *
+ * @author Przemek
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+//        BazaDanych bazaDanych = new BazaDanych();
+//        bazaDanych.baza();
+
+        BazaFrame bf=new BazaFrame();
+        bf.setTitle("Aplikacja -lab 1");
+        bf.setSize(640,480);
+        bf.setVisible(true);
+        
+    }
+    
+    
+    
+    
+    
+}
